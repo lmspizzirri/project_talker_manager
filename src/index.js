e11f9670be689a12cbba641764c19e7edfd63364
Lucas <lm.spizzirri@gmail.com>
2023-05-22 22:51:44 -0300

    Iniciando Projeto/ Criação GET Talkers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const fs = require('fs/promises');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+const path_file = '/talker.json'
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (req, res) => {
+  const data = await fs.readFile(path.resolve(`${__dirname}/${path_file}`), 'utf-8')
+  const talkers = JSON.parse(data);
+  return res.status(200).json( talkers );
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+module.exports = app;
