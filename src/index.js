a8024a7ab401eb5d8d0891d0499b084f8ff42bd3
Lucas <lm.spizzirri@gmail.com>
2023-05-24 16:15:26 -0300

    Validação Email e Password

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs/promises');
 const path = require('path');
+const { PassThrough } = require('stream');
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,27 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 const path_file = '/talker.json'
+var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+
+function isEmailValid(email) {
+    if (!email)
+        return false;
+    var valid = emailRegex.test(email);
+    if(!valid)
+        return false;
+    return true;
+}
+
+function tokenGenerator(){
+  let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let counter = 0;
+    while (counter < 16) {
+      result += characters.charAt(Math.floor(Math.random() * 16));
+      counter += 1;
+    }
+  return result;
+}
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -36,17 +58,43 @@ app.get('/talker/:id', async (req, res) => {
 
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let counter = 0;
-  while (counter < 16) {
-    result += characters.charAt(Math.floor(Math.random() * 16));
-    counter += 1;
+  let emailLength = 0;
+  let passwordLength = 0;
+  let passwordVerify;
+  let emailVerify;
+  if ('email' in req.body){
+    emailLength = email.length;
+    emailVerify = isEmailValid(email);
   }
-  console.log(result);
-  return res.status(200).json({
-    "token": result
-  })
+  if ('password' in req.body){
+    passwordLength = password.length;
+    passwordVerify = passwordLength > 6;
+  }
+  switch (true) {
+    case (passwordLength === 0) :
+      return res.status(400).json({
+        "message": "O campo \"password\" é obrigatório"
+      })
+    case (passwordVerify === false) :
+      return res.status(400).json({
+        "message": "O \"password\" deve ter pelo menos 6 caracteres"
+      })
+    case (emailLength === 0) :
+      return res.status(400).json({
+        "message": "O campo \"email\" é obrigatório"
+      })
+    case (emailVerify === false) :
+      return res.status(400).json({
+        "message": "O \"email\" deve ter o formato \"email@email.com\""
+      })
+    case (passwordVerify && emailVerify) :
+      const result = tokenGenerator();
+      return res.status(200).json({
+        "token": result
+      })
+    default:
+      return res.status(500);
+  }
 });
 
 app.listen(PORT, () => {
